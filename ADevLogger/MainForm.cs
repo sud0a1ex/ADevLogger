22eefd9a3c1e3e919fcf83bad5b05d70bca5229a
sud0a1ex <116053566+sud0a1ex@users.noreply.github.com>
2026-07-30 00:15:54 +0700

    Added comments

--- ADevLogger/MainForm.cs
+++ ADevLogger/MainForm.cs
@@ -9,17 +9,20 @@ namespace ADevLogger
             InitializeComponent();
         }
 
+        // Open "New Project" window
         private void ButtonNew_Click(object sender, EventArgs e)
         {
             new NewProject().ShowDialog();
         }
 
+        // Update log count(variable and label)
         void UpdateLogCount()
         {
             LogCount += 1;
             LabelLogCount.Text = $"Log Count: {LogCount}";
         }
 
+        // Update info labels(log count, dlog path, and markdown path)
         void UpdateInfoLabels()
         {
             LabelLogCount.Text = $"Log Count: {LogCount}";
@@ -27,38 +30,52 @@ namespace ADevLogger
             LabelPathMarkdown.Text = $"File Path(.md): {MarkdownPath}";
         }
 
+        // Check project loader(.dlog file)
         void CheckProjectLoader(string FilePath)
         {
+            // Read text inside project loader
             StreamReader ReadProject = new(FilePath);
             string[] ProjectLoader = ReadProject.ReadToEnd().Split("\n");
             ReadProject.Close();
+
+            // Check project loader format
             if (!ProjectLoader[0].StartsWith("log:") || !ProjectLoader[1].StartsWith("markdown:") || !char.IsDigit(ProjectLoader[0].Remove(0, 4)[0]))
                 MessageBox.Show("Invalid project loader.", "ADevLogger", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            LogCount = int.Parse(ProjectLoader[0].Remove(0, 4));
-            MarkdownPath = ProjectLoader[1].Remove(0, 9);
-            UpdateInfoLabels();
-            splitContainer1.Enabled = true;
+
+            // If format is correct, continue
+            // Parse info from project loader
+            LogCount = int.Parse(ProjectLoader[0].Remove(0, 4)); // Get latest log count
+            MarkdownPath = ProjectLoader[1].Remove(0, 9); // Get markdown path
+            UpdateInfoLabels(); // Update to labels
+            splitContainer1.Enabled = true; // Enable editor
+
+            // Read markdown
             StreamReader ReadMarkdown = new(MarkdownPath);
             string Markdown = ReadMarkdown.ReadToEnd();
             ReadMarkdown.Close();
-            BoxCurrentLog.Text = Markdown;
+            BoxCurrentLog.Text = Markdown; // Set current log textbox to text from markdown
         }
 
+        // Open a project
         private void ButtonOpen_Click(object sender, EventArgs e)
         {
+            // Open file browser
             OpenFileDialog FileBrowser = new()
             {
                 Title = "Select ADevLogger project loader.",
                 Filter = "ADevLogger (*.dlog)|*.dlog"
             };
             FileBrowser.ShowDialog();
+
+            // Check if file is not null
             if (!string.IsNullOrEmpty(FileBrowser.FileName))
             {
-                ProjectPath = FileBrowser.FileName;
-                CheckProjectLoader(ProjectPath);
+                ProjectPath = FileBrowser.FileName; // Set project path to selected file
+                CheckProjectLoader(ProjectPath); // Send file to check if valid
             }
         }
 
+        // Add log to current log textbox
         private void ButtonAdd_Click(object sender, EventArgs e)
         {
             UpdateLogCount();
@@ -67,17 +84,23 @@ namespace ADevLogger
             BoxTopic.Text = BoxContent.Text = "";
         }
 
+        // Save updated log
         private void ButtonSave_Click(object sender, EventArgs e)
         {
+            // Check if a project is opened
             if(!string.IsNullOrEmpty(ProjectPath) && !string.IsNullOrEmpty(MarkdownPath))
             {
+                // Rewrite project loader with new log count
                 StreamWriter RewriteLoader = new(ProjectPath);
                 RewriteLoader.Write($"log:{LogCount}\nmarkdown:{MarkdownPath}");
                 RewriteLoader.Close();
 
+                // Rewrite markdown with new log
                 StreamWriter RewriteMarkdown = new(MarkdownPath);
                 RewriteMarkdown.Write(BoxCurrentLog.Text);
                 RewriteMarkdown.Close();
+
+                MessageBox.Show("Project saved successfully!", "ADevLogger");
             }
         }
     }
--- ADevLogger/NewProject.cs
+++ ADevLogger/NewProject.cs
@@ -8,6 +8,7 @@
             InitializeComponent();
         }
 
+        // Update info label
         void UpdateInfoLabel()
         {
             LabelInfo.Text = "Project Info:\n";
@@ -17,35 +18,43 @@
                 LabelInfo.Text += $"Markdown Path: {MarkdownPath}";
         }
 
+        // Selecting a directory
         private void ButtonDirectory_Click(object sender, EventArgs e)
         {
+            // Open folder browser
             FolderBrowserDialog FolderBrowser = new()
             {
                 UseDescriptionForTitle = true,
                 Description = "Select a folder for ADevLogger project."
             };
             FolderBrowser.ShowDialog();
-            DirectoryPath = FolderBrowser.SelectedPath;
-            UpdateInfoLabel();
+            DirectoryPath = FolderBrowser.SelectedPath; // Set selected folder
+            UpdateInfoLabel(); // Update the labels
         }
 
+        // Selecting a markdown file
         private void ButtonMarkdown_Click(object sender, EventArgs e)
         {
+            // Open file browser
             OpenFileDialog FileBrowser = new()
             {
                 Title = "Select a Markdown file for ADevLogger to update.",
                 Filter = "Markdown (*.md)|*.md"
             };
             FileBrowser.ShowDialog();
-            MarkdownPath = FileBrowser.FileName;
-            UpdateInfoLabel();
+            MarkdownPath = FileBrowser.FileName; // Set selected markdown file
+            UpdateInfoLabel(); // Update the labels
         }
 
+        // Create project
         private void ButtonCreate_Click(object sender, EventArgs e)
         {
+            // Project loader's path
             string ProjectLoaderFile = @$"{DirectoryPath}\{BoxProjectName.Text}.dlog";
+
+            // Create project loader
             StreamWriter ProjectLoader = new(ProjectLoaderFile);
-            ProjectLoader.Write($"log:0\nmarkdown:{MarkdownPath}");
+            ProjectLoader.Write($"log:0\nmarkdown:{MarkdownPath}"); // Write log count and markdown path
             ProjectLoader.Close();
             MessageBox.Show("Project created!\nYou can now open the project in the editor.", "ADevLogger");
         }
